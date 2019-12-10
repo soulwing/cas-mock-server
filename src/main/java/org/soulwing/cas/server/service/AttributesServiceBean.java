983c2ba67898f65750802fbb73e0fd56e03ad97f
Carl Harris <ceharris@vt.edu>
2019-12-10 13:59:00 -0500

    reduce logging noise

--- src/main/java/org/soulwing/cas/server/service/AttributesServiceBean.java
+++ src/main/java/org/soulwing/cas/server/service/AttributesServiceBean.java
@@ -77,7 +77,7 @@ class AttributesServiceBean implements AttributesService {
   @Override
   public List<AttributeValue> getAttributes(String username) {
     final List<AttributeValue> attributes = delegate.getAttributes(username);
-    logger.info("attributes released for " + username + ": " + attributes);
+    logger.fine("attributes released for " + username + ": " + attributes);
     return attributes;
   }
 
